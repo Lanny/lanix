b8820a39513f1049690509d57f2b4a5e4cd8eb94
Lanny <lan.rogers.book@gmail.com>
2022-11-26 18:21:02 -0600

    cleanup.

--- home.nix
+++ home.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, ... }:
-
-let
-  appLauncher = import ./app-launcher.nix;
-in {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+{
   home.username = "lanny";
   home.homeDirectory = "/home/lanny";
 
@@ -13,14 +8,6 @@ in {
     pkgs.xclip
   ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.05";
 
   home.sessionVariables = {
